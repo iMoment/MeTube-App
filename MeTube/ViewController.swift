44525fe8cdab86deab06fef9dc112a5404d4c93a
Stanley Pan <stanleypan.egre@gmail.com>
2016-08-30 03:49:41 +0900

    Initial Commit

--- MeTube/ViewController.swift
+++ MeTube/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Stanley Pan on 8/30/16.
 //  Copyright © 2016 Stanley Pan. All rights reserved.
-//
+
 
 import UIKit
 
@@ -14,12 +14,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
