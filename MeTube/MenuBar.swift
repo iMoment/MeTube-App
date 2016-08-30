38ceb05c650b0c964d3bbf07fcb3e2609e83ecb1
Stanley Pan <stanleypan.egre@gmail.com>
2016-08-30 19:43:32 +0900

    Implement some required delegate functions

--- MeTube/MenuBar.swift
+++ MeTube/MenuBar.swift
@@ -8,15 +8,69 @@
 
 import UIKit
 
-class MenuBar: UIView {
+class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.rgb(230, green: 32, blue: 31)
+        cv.dataSource = self
+        cv.delegate = self
+        
+        return cv
+    }()
+    
+    let cellId = "cellId"
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = UIColor.rgb(230, green: 32, blue: 31)
+        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
+        addSubview(collectionView)
+        addConstraintsWithFormat("H:|[v0]|", views: collectionView)
+        addConstraintsWithFormat("V:|[v0]|", views: collectionView)
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath)
+        
+        cell.backgroundColor = UIColor.blueColor()
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(frame.width / 4, frame.height)
+    }
+    
+    // Gets rid of spacing between items that is pushing items in collection view down
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
