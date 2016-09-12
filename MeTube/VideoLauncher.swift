16c576a1bd2eb2c2ac0c777c12a66d550d9f398b
Stanley Pan <stanleypan.egre@gmail.com>
2016-09-12 20:03:15 +0900

    Get sample video to run in player

--- MeTube/VideoLauncher.swift
+++ MeTube/VideoLauncher.swift
@@ -7,6 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
+
+class VideoPlayerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.blackColor()
+        
+        let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
+        if let url = NSURL(string: urlString) {
+            let player = AVPlayer(URL: url)
+            let playerLayer = AVPlayerLayer(player: player)
+            
+            self.layer.addSublayer(playerLayer)
+            playerLayer.frame = self.frame
+            
+            player.play()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
 class VideoLauncher: NSObject {
     
@@ -15,9 +40,14 @@ class VideoLauncher: NSObject {
         
         if let keyWindow = UIApplication.sharedApplication().keyWindow {
             let view = UIView(frame: keyWindow.frame)
-            view.backgroundColor = UIColor.redColor()
+            view.backgroundColor = UIColor.whiteColor()
             
             view.frame = CGRect(x: keyWindow.frame.width - 10, y: keyWindow.frame.height - 10, width: 10, height: 10)
+            
+            let height = keyWindow.frame.width * 9 / 16
+            let videoPlayerFrame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
+            let videoPlayerView = VideoPlayerView(frame: videoPlayerFrame)
+            view.addSubview(videoPlayerView)
             
             keyWindow.addSubview(view)
             
@@ -27,6 +57,7 @@ class VideoLauncher: NSObject {
                 
                 }, completion: { (completedAnimation) in
                     // We'll do something here later
+                    UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: .Fade)
             })
         }
     }
